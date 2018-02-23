38f99c0ef99a89c0bbf31a1ea237074cf8d605e8
Vladimír Kriška <vlado@keboola.com>
2018-02-23 19:05:25 +0100

    Build docs for master and tags

--- .sami-config.php
+++ .sami-config.php
@@ -1,6 +1,7 @@
 <?php declare(strict_types = 1);
 
 use Sami\Sami;
+use Sami\Version\GitVersionCollection;
 use Symfony\Component\Finder\Finder;
 
 $iterator = Finder::create()
@@ -8,9 +9,15 @@ $iterator = Finder::create()
     ->name('*.php')
     ->in($dir = __DIR__ . '/src');
 
+$versions = GitVersionCollection::create($dir)
+    ->addFromTags('*')
+    ->add('master', 'master branch')
+;
+
 return new Sami($iterator, [
     'title' => 'php-docker-application',
-    'build_dir' => __DIR__ . '/docs/',
-    'cache_dir' => __DIR__ . '/cache/',
+    'versions'             => $versions,
+    'build_dir' => __DIR__ . '/docs/%version%',
+    'cache_dir' => __DIR__ . '/cache/%version%',
     'default_opened_level' => 2,
 ]);
