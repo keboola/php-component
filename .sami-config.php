edf17b785f910197142369ad38f0e6eec76b270d
Tomáš Fejfar <tomas.fejfar@gmail.com>
2018-02-23 17:03:39 +0000

    Remove dead code from docs config

--- .sami-config.php
+++ .sami-config.php
@@ -1,19 +1,12 @@
 <?php declare(strict_types = 1);
 
 use Sami\Sami;
-use Sami\Version\GitVersionCollection;
 use Symfony\Component\Finder\Finder;
 
 $iterator = Finder::create()
     ->files()
     ->name('*.php')
     ->in($dir = __DIR__ . '/src');
-
-/*$versions = GitVersionCollection::create($dir)
-//    ->addFromTags('v2.0.*')
-//    ->add('2.0', '2.0 branch')
-    ->add('master', 'master branch')
-;*/
 
 return new Sami($iterator, [
     'title' => 'php-docker-application',
