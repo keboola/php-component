0146a874cea77983c7084eb8840c2872a9121bc1
Tomáš Fejfar <tomas.fejfar@gmail.com>
2018-03-05 12:03:06 +0000

    Fix reported phpcs/phpstan issues in example files

--- example/MyConfig.php
+++ example/MyConfig.php
@@ -1,8 +1,10 @@
-<?php
+<?php declare(strict_types = 1);
+
+namespace MyComponent;
 
 class MyConfig extends \Keboola\Component\Config\BaseConfig
 {
-    public function getMaximumAllowedErrorCount()
+    public function getMaximumAllowedErrorCount(): int
     {
         $defaultValue = 0;
         return $this->getValue(['parameters', 'errorCount', 'maximumAllowed'], $defaultValue);
